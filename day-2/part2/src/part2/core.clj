e9378815aecfe488800e27380e2bde33cd5ea737
Tyler Wilding <xtvaser@gmail.com>
2017-12-11 00:46:23 -0500

    day2-part2 completed

--- day-2/part2/src/part2/core.clj
+++ day-2/part2/src/part2/core.clj
@@ -1,7 +1,25 @@
 (ns part2.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.math.combinatorics :as combo]))
+
+(defn read-input [file]
+  (for [line (clojure.string/split-lines (slurp file))]
+    (->> (clojure.string/split line #"\t")
+         (map #(Integer/parseInt %))
+         (conj))))
+
+(defn divide-pair [pair]
+  (let [division-result (/ (apply max pair) (apply min pair))]
+    (if (integer? division-result)
+      division-result
+      nil)))
+
+(defn find-divisible [row]
+  (first
+    (remove nil?
+            (map divide-pair (combo/combinations row 2)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [file]
+  (apply + (map find-divisible (read-input file))))
