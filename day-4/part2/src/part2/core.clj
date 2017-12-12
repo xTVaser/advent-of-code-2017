8ae2c0048eb75ccb53fc8dd060d88856a0ae0d5b
Tyler Wilding <xtvaser@gmail.com>
2017-12-11 22:54:23 -0500

    day4-part2 completed

--- day-4/part2/src/part2/core.clj
+++ day-4/part2/src/part2/core.clj
@@ -1,7 +1,23 @@
 (ns part2.core
   (:gen-class))
 
+(defn sort-string [word]
+  (apply str (sort word)))
+
+(defn read-input [file]
+  (for [line (clojure.string/split-lines (slurp "resources/input"))]
+    (->> (clojure.string/split line #" ")
+         (map sort-string)
+         (conj))))
+
+(defn check-valid [row]
+  (if (= (count row) (count (distinct row)))
+    row
+    nil))
+
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [file]
+  (count
+    (remove nil?
+            (map check-valid (read-input file)))))
