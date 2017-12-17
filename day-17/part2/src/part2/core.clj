11a44ad626d4d3eedd933d06a938278b917e9132
Tyler Wilding <xtvaser@gmail.com>
2017-12-17 16:14:49 -0500

    day17-part2 completed

--- day-17/part2/src/part2/core.clj
+++ day-17/part2/src/part2/core.clj
@@ -1,7 +1,17 @@
 (ns part2.core
   (:gen-class))
 
+(defn spinlock [insertions steps]
+  (loop [index 0
+         fakeSize 1
+         val 1]
+    (when (<= val insertions)
+      (let [at (inc (mod (+ index steps) fakeSize))]
+        (when (= at 1) ; only care when updating the value next to 0
+          (println val))
+        (recur at (inc fakeSize) (inc val))))))
+
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [steps]
+  (spinlock 50000000 steps))
